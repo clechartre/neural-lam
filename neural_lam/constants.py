8847c2ed09c6e629738b290f068272fbe58b165e
Simon Adamov <simon.adamov@meteoswiss.ch>
2023-11-16 16:06:51 +0100

    More user defined input

--- neural_lam/constants.py
+++ neural_lam/constants.py
@@ -24,22 +24,26 @@ param_units = [
 ]
 # Projection and grid
 grid_shape = (2632, 64)  # original (x, y)
+dm_scale = 0.05  # radius within which grid nodes are associated with a mesh node
 
 # Zoom for graph plotting
-zoom_limit = 128
+zoom_limit = 1e10
 
 # Time step prediction during training / prediction (eval)
-init_time = 20 # 10s steps (after bubble drop)
+init_time = 20  # 10s steps (after bubble drop)
+end_time = 120  # 10s steps (before bubble drop)
 train_horizon = 3  # 10s steps (t-1 + t -> t+1)
-eval_horizon = 80  # 10s steps (autoregressive)
+eval_horizon = 12  # 10s steps (autoregressive)
+
+# Training
+weighted_loss = False
+val_step_log_errors = np.arange(1, eval_horizon - 1)
+metrics_initialized = False
 
 # Plotting
 fig_size = (15, 10)
 example_file = "data/straka/samples/test/data_test.zarr"
-
-# Log prediction error for these time steps forward
-val_step_log_errors = np.arange(1, eval_horizon - 1, 10)
-metrics_initialized = False
+store_example_data = False
 
 # Some constants useful for sub-classes
 batch_static_feature_dim = 0
