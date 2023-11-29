ea2575b7dd0a00397ce0aa426128ee1a48206ed1
Simon Adamov <simon.adamov@meteoswiss.ch>
2023-11-29 12:41:28 +0100

    fixed loop for gifs

--- create_gifs.sh
+++ create_gifs.sh
@@ -2,12 +2,15 @@
 
 # Define the parameters and levels
 params=("theta_v")
+member=("1" "2" "3" "4" "5" "6" "7" "8" "9")
 
 # Get the output directory from the first script argument
 output_dir=$1
 
 # Loop over all combinations
 for param in "${params[@]}"; do
-    # Generate the GIF
-    convert -delay 20 -loop 0 ${output_dir}/${param}_* ${output_dir}/${param}.gif
+    for member in "${member[@]}"; do
+        # Generate the GIF
+        convert -delay 100 -loop 0 ${output_dir}/${param}_m_${member}_t* ${output_dir}/${param}_m_${member}.gif
+    done
 done
